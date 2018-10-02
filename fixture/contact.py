4ef128ce36a23e5523f79823ac454fd3f5b45353
kesbo <kesbo@users.noreply.github.com>
2018-10-02 21:46:19 +0300

    Выделен класс для работы с контактами

---
+++ fixture/contact.py
@@ -0,0 +1,22 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_contact_page()
+        # fill contact from
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        # submit creation contact
+        wd.find_element_by_name("submit").click()
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
